f952a2f97d555a17d23197d2400b89145febec98
root <root@ip-10-0-3-28.us-west-2.compute.internal>
2019-06-14 09:00:01 +0000

    Added gitignore

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "account_id" {
+ type = "string"
+ default = "538853303114"
+}
+
+variable "region" {
+ type = "string"
+ default = "us-west-2"
+}
+
+variable "credentials_filepath" {
+ type = "string"
+ default = "/root/.aws/credentials"
+}
